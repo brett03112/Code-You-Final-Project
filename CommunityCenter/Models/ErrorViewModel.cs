f86ac5f51f1d506cc8feb2cd6eed474f52a61cf1
Brett Smith <brett03112@gmail.com>
2024-12-04 18:34:20 -0500

    Changed name of project to CommunityCenter

---
+++ CommunityCenter/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CommunityCenter.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
