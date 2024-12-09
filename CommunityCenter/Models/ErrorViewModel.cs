c34d9a522d94fdfb479f22388c7f64da3482252f
Brett Smith <brett03112@gmail.com>
2024-12-09 16:50:24 -0500

    Refactor project structure to use Blazor WebAssembly (WASM) for client-side rendering and Minimal API for server-side API, enabling better implementation of real-time functionality with SignalR

--- CommunityCenter/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace CommunityCenter.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
